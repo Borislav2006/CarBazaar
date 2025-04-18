292f42437bd2ac368413794b83ca555b5fb3a4b1
Borislav Ivanov <borislav.project@gmail.com>
2025-04-18 17:14:00 +0300

    feat: add new queryObject

---
+++ Helper/QueryObject.cs
@@ -0,0 +1,15 @@
+﻿namespace CarBazaar.Helper
+{
+    public class QueryObject
+    {
+        public string? Brand { get; set; } = null;
+        public string? Model { get; set; } = null;
+        public string? EngineType { get; set; } = null;
+        public string? GearBox { get; set; } = null;
+        public string? Color { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false;
+        public int  PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
