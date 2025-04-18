8ccced2d1361229fc80ca168ded28a8274607e9c
Borislav Ivanov <borislav.project@gmail.com>
2025-04-18 17:11:52 +0300

    feat: add new Context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarBazaar.Models;
 
-public partial class AppDbContext : DbContext
+public partial class AppDbContext : IdentityDbContext<User>
 {
     public AppDbContext()
     {
@@ -19,5 +20,12 @@ public partial class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Listing> Listings { get; set; }
     public DbSet<ListingImage> ListingImages { get; set; }
-
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<ListingImage>()
+            .HasOne(li => li.Listing) // Each ListingImage has one Listing
+            .WithMany(l => l.ListingImages) // Each Listing can have many ListingImages
+            .HasForeignKey(li => li.ListingId); // Foreign key in ListingImage pointing to Listing
+    }
 }
