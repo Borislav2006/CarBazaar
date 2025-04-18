6a553c3b077e31fb67bcdfb9f950fdba0c7b8352
Borislav Ivanov <borislav.project@gmail.com>
2025-04-18 17:13:08 +0300

    add new repositories

---
+++ Repository/ListingRespository.cs
@@ -0,0 +1,142 @@
+﻿using CarBazaar.Dtos.Listing;
+using CarBazaar.Models;
+using CarBazaar.Interface;
+using Microsoft.EntityFrameworkCore;
+using CarBazaar.Helper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarBazaar.Repository
+{
+    public class ListingRespository : IListingRepository
+    {
+        private readonly AppDbContext DbContext;
+        public ListingRespository(AppDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+
+        public async Task<Listing> CreateListingAsync(Listing listing)
+        {
+            await DbContext.Listings.AddAsync(listing);
+            await DbContext.SaveChangesAsync();
+            return listing;
+        }
+
+        public async Task<Listing> DeleteListingAsync(int id)
+        {
+            var listing = await DbContext.Listings.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (listing == null)
+            {
+                return null;
+            }
+            DbContext.Listings.Remove(listing);
+            return listing;
+        }
+
+        public async Task<List<Listing>> GetAllAsync(QueryObject query)
+        {
+            var listings = DbContext.Listings.Include(i => i.ListingImages).Include(s => s.Seller).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(query.Brand))
+            {
+                listings = listings.Where(s => s.Brand == query.Brand);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.Model))
+            {
+                listings = listings.Where(s => s.Model == query.Model);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.EngineType))
+            {
+                listings = listings.Where(s => s.EngineType == query.EngineType);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.GearBox))
+            {
+                listings = listings.Where(s => s.GearBox == query.GearBox);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.Color))
+            {
+                listings = listings.Where(s => s.Color == query.Color);
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Brand", StringComparison.OrdinalIgnoreCase))
+                {
+                    listings = query.IsDecsending ? listings.OrderByDescending(s => s.Brand) : listings.OrderBy(s => s.Brand);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Model", StringComparison.OrdinalIgnoreCase))
+                {
+                    listings = query.IsDecsending ? listings.OrderByDescending(s => s.Model) : listings.OrderBy(s => s.Model);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Price", StringComparison.OrdinalIgnoreCase))
+                {
+                    listings = query.IsDecsending ? listings.OrderByDescending(s => s.Price) : listings.OrderBy(s => s.Price);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Created", StringComparison.OrdinalIgnoreCase))
+                {
+                    listings = query.IsDecsending ? listings.OrderByDescending(s => s.CreatedAt) : listings.OrderBy(s => s.CreatedAt);
+                }
+            }
+            // SortBy:Model,Price,Brand,CreatedTime
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await listings.Skip(skipNumber).Take(query.PageSize).ToListAsync();
+        }
+
+        public async Task<Listing?> GetByIdAsync(int id)
+        {
+           var listing = await DbContext.Listings.AsNoTracking().Include(l => l.ListingImages).Include(s => s.Seller).FirstOrDefaultAsync(i => i.Id == id);
+            Console.WriteLine("Test: " + listing.ToString());
+            return listing;
+        }
+
+        public Task<bool> SellerExist(int id)
+        {
+            return DbContext.Listings.AnyAsync(x => x.Id == id);
+        }
+
+        public async Task<Listing> UpdateListingAsync(int id, Listing lisitngDto)
+        {
+            var existingLisitng = await DbContext.Listings.FindAsync(id);
+
+            if (existingLisitng == null)
+            {
+                return null;
+            }
+
+            existingLisitng.Title = lisitngDto.Title;
+            existingLisitng.Description = lisitngDto.Description;
+            existingLisitng.Brand = lisitngDto.Brand;
+            existingLisitng.Model = lisitngDto.Model;
+            existingLisitng.Year = lisitngDto.Year;
+            existingLisitng.Milage = lisitngDto.Milage;
+            existingLisitng.Price = lisitngDto.Price;
+            existingLisitng.EngineType = lisitngDto.EngineType;
+            existingLisitng.HorsePower = lisitngDto.HorsePower;
+            existingLisitng.GearBox = lisitngDto.GearBox;
+            existingLisitng.Color = lisitngDto.Color;
+
+            await DbContext.SaveChangesAsync();
+
+            return existingLisitng;
+        }
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿//using CarBazaar.Dtos.User;
+//using CarBazaar.Models;
+//using CarBazaar.Interface;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace CarBazaar.Repository
+//{
+//    public class UserRepository : IUserRepository
+//    {
+//        private readonly AppDbContext DbContext;
+//        public UserRepository(AppDbContext DbContext)
+//        {
+//            this.DbContext = DbContext;
+//        }
+
+//        public async Task<User?> GetUserByEmailAsync(string email)
+//        {
+//            return await DbContext.Users.FirstOrDefaultAsync(i => i.Email == email);
+//        }
+
+//        public async Task<User?> GetUserByIdAsync(int id)
+//        {
+//            return await DbContext.Users.Include(c => c.Listings).FirstOrDefaultAsync(i => i.Id == id);
+//        }
+
+//        public async Task<User> UpdateUserAsync(int id, UpdateUserRequestDto userDto)
+//        {
+
+//            var existingUser = await DbContext.Users.FindAsync(id);
+
+//            if (existingUser == null)
+//            {
+//                return null;
+//            }
+
+//            existingUser.FirstName = userDto.FirstName;
+//            existingUser.LastName = userDto.LastName;
+//            existingUser.PhoneNumber = userDto.PhoneNumber;
+//            existingUser.PasswordHash = userDto.Password;
+
+//            await DbContext.SaveChangesAsync();
+//            return existingUser;
+//        }
+
+//    }
+//}
